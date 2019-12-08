9ff03560a5fe3cdab379b6ff619499a8d4531d62
Reeder <reederloveland@gmail.com>
2019-12-07 22:55:08 -0800

    fixit commit

---
+++ Project1xml/app/src/main/java/com/cpsc411/project1xml/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cpsc411.project1xml;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
